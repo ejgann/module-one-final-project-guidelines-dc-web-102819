2cc39e8d595a36b6e90f48701ee8b4eb6a9606a2
ejgann <aspentree79@gmail.com>
2019-11-11 15:21:09 -0500

    file creation and setup

--- Rakefile
+++ Rakefile
@@ -1,5 +1,6 @@
 require_relative 'config/environment'
-require 'sinatra/activerecord/rake'
+
+
 
 desc 'starts a console'
 task :console do
--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,13 @@
 require 'bundler'
 Bundler.require
 
+require './bin/playlist.rb'
+require './bin/song.rb'
+require './bin/song_playlist.rb'
+
+require 'sinatra/activerecord/rake'
+
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
 require_all 'lib'
+
+ActiveRecord::Base.logger = Logger.new(STDOUT)
