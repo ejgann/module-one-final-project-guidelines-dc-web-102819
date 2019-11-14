609c26c54f61f3523a128537632e2c100e8192d9
ejgann <aspentree79@gmail.com>
2019-11-14 11:44:45 -0500

    it's working

--- bin/run.rb
+++ bin/run.rb
@@ -12,9 +12,8 @@ until restart == "no" do
     Cli.activity
     activity_input = gets.chomp
     title = gets.chomp
-    puts "\n"
-    # Cli.playlist_name(min_tempo, max_tempo)
-    Cli.playlist.create(title, min_tempo, max_tempo)
+    Song.add_song
+    
     
     # Cli.get_130_140_songs(tempo)
     
--- lib/song.rb
+++ lib/song.rb
@@ -3,5 +3,12 @@ class Song < ActiveRecord::Base
     has_many :song_playlists
     has_many :playlists, through: :song_playlists
 
+    def self.get_songs_in_range(min, max)
+        Song.all.select do |song|
+        song.tempo >= 130 && song.tempo <= 140
+        end
+    end
+    
+
 end
 
